e79358af8d970ad15c5b608286193e833de9387a
Navkcalbeht32 <asumoski@gmail.com>
2021-03-19 13:09:28 -0400

    color change

--- components/Waves.js
+++ components/Waves.js
@@ -7,8 +7,8 @@ const Waves = () => {
             <defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" /></defs>
             <g class="parallax">
                 <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(223, 46, 102, 0.3)" />
-                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(130, 88, 210, 0.3)" />
-                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(143,36,236,0.3)" />
+                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(130, 88, 210, 0.5)" />
+                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(143,36,236,0.5)" />
                 <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
             </g>
             </svg>
